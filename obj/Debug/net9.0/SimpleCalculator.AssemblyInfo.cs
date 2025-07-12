74342f5a03078afc8c47b7022fb6ea8c5b53ff75
Chathuni Sooriyaarachchi <chathuni.20230146@iit.ac.lk>
2025-07-12 18:00:05 +0530

    firt commit

--- obj/Debug/net9.0/SimpleCalculator.AssemblyInfo.cs
+++ obj/Debug/net9.0/SimpleCalculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a07289425ec65ba8660cd5f1f128423b6eccc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
